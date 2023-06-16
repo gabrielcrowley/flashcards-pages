2dde2eeb82eb66e76d9b919d950d2c42dbaa2610
Gabe Crowley <gabrielcrowley@gmail.com>
2023-06-16 16:22:21 -0500

    Added tRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,24 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { trpc } from "@/utils/trpc";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const hello = trpc.hello.useQuery({ text: 'tRPC' });
+  if (!hello.data) {
+    return <div>Loading...</div>
+  }
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
     >
       <Head>
         <title>Flashcards</title>
       </Head>
       <h1 className={`text-3xl font-bold`}>Flashcards</h1>
+      <p>{hello.data.greeting}</p>
     </main>
   );
 }
